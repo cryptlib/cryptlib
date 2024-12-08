a94b22fb7bd960e6c244452291dbb1af55b9fd56
fencepost-error <pgut001@cs.auckland.ac.nz>
2024-12-09 03:52:58 +1300

    Initial upload

---
+++ SECURITY.md
@@ -0,0 +1,7 @@
+# Security Policy
+
+As part of our support for the OpenSSL Mission we also support the [OpenSSL
+security policy](https://openssl-library.org/policies/general/security-policy/index.html),
+applied for cryptlib.  To report a vulnerability for cryptlib, use the "Report
+a Vulnerability" button in the 
+[Security section](https://github.com/cryptlib/cryptlib/security).
---
+++ bindings/setup.py
@@ -0,0 +1,37 @@
+<<<<<<< HEAD
+#!/usr/bin/env python
+
+from setuptools import setup, Extension
+import sys
+
+if sys.platform == "win32":
+    ext = Extension("cryptlib_py",
+                    sources=["python.c"],
+                    library_dirs=['../binaries'],
+                    libraries=['cl32'])
+else:
+    ext = Extension("cryptlib_py",
+                    sources=["python.c"],
+                    library_dirs=['..'],
+                    libraries=['cl'])
+
+setup(name="cryptlib_py", ext_modules=[ext])
+=======
+#!/usr/bin/env python
+
+from setuptools import setup, Extension
+import sys
+
+if sys.platform == "win32":
+    ext = Extension("cryptlib_py",
+                    sources=["python.c"],
+                    library_dirs=['../binaries'],
+                    libraries=['cl32'])
+else:
+    ext = Extension("cryptlib_py",
+                    sources=["python.c"],
+                    library_dirs=['..'],
+                    libraries=['cl'])
+
+setup(name="cryptlib_py", ext_modules=[ext])
+>>>>>>> c627b7fdce5a7d3fb5a3cfac7f910c556c3573ae
