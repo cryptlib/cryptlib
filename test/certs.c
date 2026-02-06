7db94d8d4e3c9c17e71a89bd49636cb598c28949
safewebdrop <code@senderek.ie>
2026-02-06 10:53:09 +0100

    Update CERTTIME_DATETEST to 2026
    Updating CERTTIME_DATETEST to 2026 prevents stestlib to terminate with "Error: CERTTIME_DATETEST must be > MIN_TIME_VALUE.
    Test of testCACert() failed."
    
    Uh, time flies!
    
    Ralf

--- test/certs.c
+++ test/certs.c
@@ -50,9 +50,9 @@
 
 #define ONE_YEAR_TIME	( 365 * 86400L )
 #if defined( __MWERKS__ ) || defined( SYMANTEC_C ) || defined( __MRC__ )
-  #define CERTTIME_DATETEST	( ( ( 2024 - 1970 ) * ONE_YEAR_TIME ) + 2082844800L )
+  #define CERTTIME_DATETEST	( ( ( 2026 - 1970 ) * ONE_YEAR_TIME ) + 2082844800L )
 #else
-  #define CERTTIME_DATETEST	( ( 2024 - 1970 ) * ONE_YEAR_TIME )
+  #define CERTTIME_DATETEST	( ( 2026 - 1970 ) * ONE_YEAR_TIME )
 #endif /* Macintosh-specific weird epoch */
 #if ( ULONG_MAX > 0xFFFFFFFFUL ) || defined( _M_X64 )
   #define SYSTEM_64BIT
